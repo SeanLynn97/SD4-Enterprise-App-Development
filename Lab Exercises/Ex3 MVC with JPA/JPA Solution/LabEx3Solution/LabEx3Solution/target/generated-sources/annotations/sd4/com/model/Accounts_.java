1b9eea3586c7630485d10b1ca45b86eef792b90d
lit-alan <Alan.Ryan@lit.ie>
2020-10-07 09:48:49 +0100

    Solution Added For Lab Ex3

---
+++ Lab Exercises/Ex3 MVC with JPA/JPA Solution/LabEx3Solution/LabEx3Solution/target/generated-sources/annotations/sd4/com/model/Accounts_.java
@@ -0,0 +1,17 @@
+package sd4.com.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-06T20:24:46")
+@StaticMetamodel(Accounts.class)
+public class Accounts_ { 
+
+    public static volatile SingularAttribute<Accounts, String> firstName;
+    public static volatile SingularAttribute<Accounts, String> lastName;
+    public static volatile SingularAttribute<Accounts, Double> balance;
+    public static volatile SingularAttribute<Accounts, Integer> accountNumber;
+    public static volatile SingularAttribute<Accounts, String> sortCode;
+
+}
